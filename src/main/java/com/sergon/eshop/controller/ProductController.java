b26a0b01ac46fc729d9a08bfb1865b81380490dd
Sergio González <sgzzsifuentes@gmail.com>
2024-02-13 19:29:41 -0600

    Added Controller, Dto, Repository and Service packages to implement the creating product service.

---
+++ src/main/java/com/sergon/eshop/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.sergon.eshop.controller;
+
+import com.sergon.eshop.dto.ProductRequest;
+import com.sergon.eshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController
+{
+    private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest)
+    {
+        productService.createProduct(productRequest);
+    }
+}
---
+++ src/main/java/com/sergon/eshop/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.sergon.eshop.service;
+
+import com.sergon.eshop.dto.ProductRequest;
+import com.sergon.eshop.model.Product;
+import com.sergon.eshop.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService
+{
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest)
+    {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+}
