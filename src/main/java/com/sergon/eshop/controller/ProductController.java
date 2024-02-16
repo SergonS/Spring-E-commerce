a9518fa74eb9a5dddd66fe5e67d3df40e4a25f1a
Sergio González <sgzzsifuentes@gmail.com>
2024-02-16 16:28:41 -0600

    Added ProductResponse Dto and Get All Products method to Service

--- src/main/java/com/sergon/eshop/controller/ProductController.java
+++ src/main/java/com/sergon/eshop/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.sergon.eshop.controller;
 
 import com.sergon.eshop.dto.ProductRequest;
+import com.sergon.eshop.dto.ProductResponse;
 import com.sergon.eshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -17,5 +20,12 @@ public class ProductController
     public void createProduct(@RequestBody ProductRequest productRequest)
     {
         productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts()
+    {
+        return productService.getAllProducts();
     }
 }
--- src/main/java/com/sergon/eshop/service/ProductService.java
+++ src/main/java/com/sergon/eshop/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.sergon.eshop.service;
 
 import com.sergon.eshop.dto.ProductRequest;
+import com.sergon.eshop.dto.ProductResponse;
 import com.sergon.eshop.model.Product;
 import com.sergon.eshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +27,22 @@ public class ProductService
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts()
+    {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product)
+    {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
